1ff91dfba6b29248048c3a3bae443b870d7146bf
GG1GG <>
2023-06-20 17:14:11 +0300

    дз1

--- dz_10_13_15.cs
+++ dz_10_13_15.cs
@@ -1,8 +1,10 @@
 ﻿// Задача 10
 Console.WriteLine("Программа принемает трех значное число и выводит вторую цифру");
+{
 int a = int.Parse(Console.ReadLine() ?? "");
 int b = (a % 10);
 int c = ((a % 100 - b) / 10);
+}
 Console.WriteLine($"Cередина данного числа   {c} " );
 
 // Задача 13
@@ -39,13 +41,17 @@ int MakeArray(int a, int b)
 // Задача 15
 Console.WriteLine("Введите число дня недели от 1 до 7: ");
 int number_a = int.Parse(Console.ReadLine() ?? "");
+
 if (number_a < 1 ^ number_a > 7)
+{
     Console.WriteLine("Вы ввели неверное число");
-
+}
 if (number_a > 5 && number_a < 8)
+{
     Console.WriteLine("Да, этот день выходной!");
-
+}
 if (number_a > 0 && number_a < 6)
+{
     Console.WriteLine("Это рабочий день! ");
-
+}
     // Алчакова Ильи
